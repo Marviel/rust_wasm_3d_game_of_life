d2d81a89bed8410c74348151e73502e3fd102bc7
marviel <lukebechtel4@gmail.com>
2021-07-09 08:54:50 -0400

    Add 3d game of life to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -126,6 +126,9 @@ impl Universe {
 /// Public methods, exported to JavaScript.
 #[wasm_bindgen]
 impl Universe {
+    pub fn test_stuff(&mut self) -> String{
+        return "Hi".to_string();
+    }
     pub fn tick(&mut self) {
         // let _timer = Timer::new("Universe::tick");
 
@@ -229,5 +232,248 @@ impl fmt::Display for Universe {
         }
 
         Ok(())
+    }
+}
+
+
+
+
+#[wasm_bindgen]
+pub struct Universe3D {
+    width: u32,
+    height: u32,
+    depth: u32,
+    cells: Vec<Cell>,
+    adjacent_counts: Vec<u8>,
+}
+
+impl Universe3D {
+    fn get_index(&self, row: u32, column: u32, lane: u32) -> usize {
+        (row * self.width + column*self.height + lane) as usize
+    }
+
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32, u32)]) {
+        for (row, col, lane) in cells.iter().cloned() {
+            let idx = self.get_index(row, col, lane);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32, lane: u32) -> u8 {
+        let mut count = 0;
+
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        let forward = if lane == self.depth - 1 {
+            0
+        } else {
+            lane + 1
+        };
+
+        let backward = if lane == 0 {
+            self.depth - 1
+        } else {
+            lane - 1
+        };
+
+        let b = self.get_index(row, column, backward);
+        count += self.cells[b] as u8;
+        
+        let f = self.get_index(row, column, forward);
+        count += self.cells[f] as u8;
+
+        let nw = self.get_index(north, west, lane);
+        count += self.cells[nw] as u8;
+        
+        let nf = self.get_index(north, column, forward);
+        count += self.cells[nf] as u8;
+
+        let nb = self.get_index(north, column, backward);
+        count += self.cells[nb] as u8;
+
+        let ne = self.get_index(north, east, lane);
+        count += self.cells[ne] as u8;
+
+        let n = self.get_index(north, column, lane);
+        count += self.cells[n] as u8;
+
+        let wf = self.get_index(row, west, forward);
+        count += self.cells[wf] as u8;
+
+        let wb = self.get_index(row, west, backward);
+        count += self.cells[wb] as u8;
+
+        let w = self.get_index(row, west, lane);
+        count += self.cells[w] as u8;
+
+        let ef = self.get_index(row, east, forward);
+        count += self.cells[ef] as u8;
+
+        let eb = self.get_index(row, east, backward);
+        count += self.cells[eb] as u8;
+
+        let e = self.get_index(row, east, lane);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west, lane);
+        count += self.cells[sw] as u8;
+
+        let sf = self.get_index(south, column, forward);
+        count += self.cells[sf] as u8;
+
+        let sb = self.get_index(south, column, backward);
+        count += self.cells[sb] as u8;
+
+        let s = self.get_index(south, column, lane);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east, lane);
+        count += self.cells[se] as u8;
+
+        count
+    }
+}
+
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Universe3D {
+    pub fn tick(&mut self) {
+        // let _timer = Timer::new("Universe::tick");
+
+        let mut next = self.cells.clone();
+        let mut adj_next = self.adjacent_counts.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                for lane in 0..self.depth {
+                    let idx = self.get_index(row, col, lane);
+                    let cell = self.cells[idx];
+                    let live_neighbors = self.live_neighbor_count(row, col, lane);
+
+                    let next_cell = match (cell, live_neighbors) {
+                        // Rule 1: Any live cell with fewer than two live neighbours
+                        // dies, as if caused by underpopulation.
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
+                        // Rule 2: Any live cell with two or three live neighbours
+                        // lives on to the next generation.
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                        // Rule 3: Any live cell with more than three live
+                        // neighbours dies, as if by overpopulation.
+                        (Cell::Alive, x) if x > 8 => Cell::Dead,
+                        // Rule 4: Any dead cell with exactly three live neighbours
+                        // becomes a live cell, as if by reproduction.
+                        (Cell::Dead, 3) => Cell::Alive,
+                        // All other cells remain in the same state.
+                        (otherwise, _) => otherwise,
+                    };
+
+                    adj_next[idx] = live_neighbors;
+                    next[idx] = next_cell;
+                }
+            }
+        }
+
+        self.cells = next;
+        self.adjacent_counts = adj_next;
+    }
+
+    pub fn new() -> Universe3D {
+        utils::set_panic_hook();
+
+        let width = 20;
+        let height = 20;
+        let depth = 20;
+
+        let cells = (0..width * height * depth)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        let adjacent_counts = (0..width * height * depth)
+            .map(|i| 0)
+            .collect();
+
+        Universe3D {
+            width,
+            height,
+            depth,
+            cells,
+            adjacent_counts,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn depth(&self) -> u32 {
+        self.depth
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn adjacent_counts(&self) -> *const u8 {
+        self.adjacent_counts.as_ptr()
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32, lane: u32) {
+        let idx = self.get_index(row, column, lane);
+        self.cells[idx].toggle();
     }
 }
